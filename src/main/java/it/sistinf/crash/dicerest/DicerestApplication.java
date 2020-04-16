0ef62ca609ebf4d030a6b133abcf3e4adc4022ef
Andrea Cirillo <Andrea.Cirillo@it.ibm.com>
2020-04-16 08:47:58 +0200

    first commit

---
+++ src/main/java/it/sistinf/crash/dicerest/DicerestApplication.java
@@ -0,0 +1,13 @@
+package it.sistinf.crash.dicerest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DicerestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DicerestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/sistinf/crash/dicerest/DicerestApplicationTests.java
@@ -0,0 +1,13 @@
+package it.sistinf.crash.dicerest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DicerestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
